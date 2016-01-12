c9f381512594c5ba03b1539dc7af887254d1f25d
unknown <aurelien.biguet@gmail.com>
2016-01-12 16:23:15 +0100

    add getters/setters Resources

---
+++ src/towerdefense/bo/Base.java
@@ -0,0 +1,7 @@
+package towerdefense.bo;
+
+/**
+ * Created by Aurélien on 12/01/2016.
+ */
+public class Base {
+}
--- src/towerdefense/bo/Ressource.java
+++ src/towerdefense/bo/Ressource.java
@@ -16,4 +16,20 @@ public class Ressource {
         this.name = name;
         this.quantity = quantity;
     }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }

fdb4ef265d98bc3d6afd4bd8b7d62cb5e1cd88a4
romain <romain.richard.19@gmail.com>
2016-01-12 17:06:37 +0100

    Add btn tower and action add one tower

--- src/towerdefense/manager/EntityManager.java
+++ src/towerdefense/manager/EntityManager.java
@@ -6,7 +6,7 @@ import towerdefense.bo.Entity;
 import java.util.ArrayList;
 
 public class EntityManager {
-    private static ArrayList<Entity> entities = new ArrayList<>();
+    public static ArrayList<Entity> entities = new ArrayList<>();
 
     public static ArrayList<Entity> getClone() {
         return (ArrayList<Entity>) entities.clone();
@@ -14,7 +14,6 @@ public class EntityManager {
 
     public static void init(){
         entities.add(new Base((UIManager.getWindow().getBoard().getWidth()/2)+100,(UIManager.getWindow().getBoard().getHeight()/2)-50, 100, 100));
-
     }
 
     public static void entityManagement() {
--- src/towerdefense/view/MainWindow.java
+++ src/towerdefense/view/MainWindow.java
@@ -1,11 +1,17 @@
 package towerdefense.view;
 
+import towerdefense.bo.Tower;
+import towerdefense.manager.EntityManager;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class MainWindow extends JFrame {
     private JPanel contentPane;
     private towerdefense.view.Board Board;
+    private JButton AddTowerBtn;
 
     public MainWindow() {
 
@@ -13,6 +19,13 @@ public class MainWindow extends JFrame {
         setVisible(true);
         setPreferredSize(new Dimension(800, 600));
         pack();
+        AddTowerBtn.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent mouseEvent) {
+                super.mouseClicked(mouseEvent);
+                EntityManager.entities.add(new Tower(50, 50, 20, 20));
+            }
+        });
     }
 
     public void refreshBoard() {

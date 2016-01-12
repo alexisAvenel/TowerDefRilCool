d28be468e65d054f9c472d0f7a485a72956046cc
florent zabera <florent.zabera@clemessy.fr>
2016-01-12 15:45:39 +0100

    add entityManagement

---
+++ src/towerdefense/bo/Ressource.java
@@ -0,0 +1,16 @@
+package towerdefense.bo;
+
+/**
+ * Created by FZAB on 12/01/2016.
+ */
+public class Ressource {
+    private String name;
+    private int quantity;
+
+    public Ressource(String name) {
+        this.name = name;
+        this.quantit
+    }
+
+    public Res
+}

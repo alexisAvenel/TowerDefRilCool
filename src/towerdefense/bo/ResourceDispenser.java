2ac0289a522bf6202ba0f1464697b48011ba6dcf
Alexis Avenel <chouk1991@gmail.com>
2016-01-13 10:01:22 +0100

    ResourceDispenser Class established with fixed size and entry point

--- src/towerdefense/bo/ResourceDispenser.java
+++ src/towerdefense/bo/ResourceDispenser.java
@@ -1,5 +1,8 @@
 package towerdefense.bo;
 
+import javafx.scene.control.Tooltip;
+import towerdefense.manager.UIManager;
+
 import java.awt.*;
 import java.util.Random;
 
@@ -8,21 +11,24 @@ import java.util.Random;
  */
 public class ResourceDispenser extends Entity {
 
-    private int quantity;
-    private int entryPointX;
-    private int entryPointY;
+    private int x;
+    private int y;
+    private static int width = 40;
+    private static int height = 40;
+    private Point p = new Point();
 
-    public ResourceDispenser(int x, int y, int width, int height, int quantity) {
-        super(x, y, width, height);
-        this.quantity = quantity;
-    }
+    public ResourceDispenser() {
 
-    public ResourceDispenser(int x, int y, int width, int height) {
-        super(x, y, width, height);
+        super(0, 0, width, height);
+        x = UIManager.getWindow().getBoard().getWidth()-80;
+        y = UIManager.getWindow().getBoard().getHeight()/2 - height/2;
+        p.setLocation(0, height/2);
     }
 
     @Override
     public void draw(Graphics2D g) {
+        x = UIManager.getWindow().getBoard().getWidth()-80;
+        y = UIManager.getWindow().getBoard().getHeight()/2 - height/2;
         g.setColor(Color.orange);
         g.fillRect(x, y, width, height);
     }
@@ -30,5 +36,9 @@ public class ResourceDispenser extends Entity {
     @Override
     public void update() {
 
+    }
+
+    public Point getP() {
+        return p;
     }
 }

87a378f589589fd53380db63b2861422b1852a37
romain <romain.richard.19@gmail.com>
2016-01-13 13:09:23 +0100

    Add picture for tower entity

--- src/towerdefense/bo/Tower.java
+++ src/towerdefense/bo/Tower.java
@@ -1,6 +1,11 @@
 package towerdefense.bo;
 
+import towerdefense.img.LogicalPath;
+
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 /**
  * Created by romain on 12/01/16.
@@ -15,8 +20,14 @@ public class Tower extends Entity {
 
     @Override
     public void draw(Graphics2D g) {
-        g.setColor(Color.black);
-        g.fillRect(x, y, width, height);
+        BufferedImage img = null;
+        try {
+            img = ImageIO.read(LogicalPath.class.getResource("tower.png"));
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
+        g.drawImage(img, x, y, width, height, null);
     }
 
     @Override

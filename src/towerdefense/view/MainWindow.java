3ce944afa4c1ca63b9f61067b414e3bab0b6be9f
romain <romain.richard.19@gmail.com>
2016-01-13 14:18:30 +0100

    Change label system message

--- src/towerdefense/view/MainWindow.java
+++ src/towerdefense/view/MainWindow.java
@@ -17,8 +17,7 @@ public class MainWindow extends JFrame {
     private JPanel contentPane;
     private towerdefense.view.Board Board;
     private JButton AddTowerBtn;
-    private JLabel systemMsgLabel;
-    private JLabel systemMsgContent;
+    private JLabel msgSystem;
     private JLabel msgNbRessources;
 
     public MainWindow() {
@@ -43,13 +42,14 @@ public class MainWindow extends JFrame {
                 if (EntityManager.getTowers().size() < 6){
                     if(((Base) EntityManager.entities.get(0)).removeRessource(new Ressource(null, Tower.cost))){
                         EntityManager.addTower(new Tower((int)(slotsTower.get(EntityManager.getTowers().size()).getX()), (int)(slotsTower.get(EntityManager.getTowers().size()).getY()), 50, 50));
+                        msgSystem.setText("System message : You add a new tower.");
                     }
                     else{
-                        systemMsgContent.setText("You can't add a tower! You don't have enough resources.");
+                        msgSystem.setText("System message : You can't add a tower! You don't have enough resources.");
                     }
                 }
                 else{
-                    systemMsgContent.setText("You can't add more tower!");
+                    msgSystem.setText("System message : You can't add more tower!");
                 }
                 msgNbRessources.setText("Ressources : "+ ((Base) EntityManager.entities.get(0)).getStock());
             }
